a090e585002256871bd89bd8962c2d949bd6750b
kakandaville <maksimantonov1994@yandex.ru>
2022-04-27 18:34:16 +0300

    Added user input + file writing, still poor but hey

--- Main_code.py
+++ Main_code.py
@@ -3,95 +3,99 @@ from bs4 import BeautifulSoup
 import lxml
 import csv
 
-url = input("Вставьте ссылку на страничку 2-недельного прогноза\n")
+with open('cities','r') as file:
+    r = file.read().split('\n')
+
+for c in r:
+    url = c
 
 
 
-headers = {
+    headers = {
 
-    "Accept": "*/*",
-    "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
-}
+        "Accept": "*/*",
+        "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
+    }
 
-req = requests.get(url, headers= headers)
-src = req.text
+    req = requests.get(url, headers= headers)
+    src = req.text
 
-# with open('index.html',encoding='utf-8') as file:
-#     src = file.read()
+    # with open('index.html',encoding='utf-8') as file:
+    #     src = file.read()
 
-soup = BeautifulSoup(src, 'lxml')
-
-
-all_days = soup.find_all(class_='day')
-all_dates = soup.find_all(class_='date')
-max_temp = soup.find_all(class_='maxt')
-min_temp = soup.find_all(class_='mint')
-conditions_raw = soup.find_all(class_='weather-icon tooltip')
-wind_units = soup.find_all(class_='wind-unit unit unit_wind_m_s')
-precipitations = soup.find(class_='widget-row widget-row-precipitation-bars row-with-caption').find_all(class_='row-item')
-title = soup.find(class_='page-title').text
-
-precips = []
-
-for item in precipitations:
-    precips.append(item.text)
-
-wind=[]
+    soup = BeautifulSoup(src, 'lxml')
 
 
-for c in wind_units:
-    wind.append(c.getText())
+    all_days = soup.find_all(class_='day')
+    all_dates = soup.find_all(class_='date')
+    max_temp = soup.find_all(class_='maxt')
+    min_temp = soup.find_all(class_='mint')
+    conditions_raw = soup.find_all(class_='weather-icon tooltip')
+    wind_units = soup.find_all(class_='wind-unit unit unit_wind_m_s')
+    precipitations = soup.find(class_='widget-row widget-row-precipitation-bars row-with-caption').find_all(class_='row-item')
+    title = soup.find(class_='page-title').text
+
+    precips = []
+
+    for item in precipitations:
+        precips.append(item.text)
+
+    wind=[]
 
 
-
-conditions=[]
-for c in conditions_raw:
-    conditions.append(c.get('data-text'))
-
-maxt = []
-for c in max_temp[:14]:
-    maxt.append(c.find(class_='unit unit_temperature_c').text)
-
-mint = []
-for c in min_temp[:14]:
-    mint.append(c.find(class_='unit unit_temperature_c').text)
+    for c in wind_units:
+        wind.append(c.getText())
 
 
 
+    conditions=[]
+    for c in conditions_raw:
+        conditions.append(c.get('data-text'))
 
-all_dates = all_dates[:14]
-all_days = all_days[:14]
-max_temp = max_temp[:14]
+    maxt = []
+    for c in max_temp[:14]:
+        maxt.append(c.find(class_='unit unit_temperature_c').text)
 
-print ('\n\n\n',title)
-for i in range(14):
-    print (f'{all_days[i].text} {all_dates[i].text} t max = {maxt[i]} t min = {mint[i]} {conditions[i]} , ветер {wind[i]} м/c , осадки {precips[i]} мм')
+    mint = []
+    for c in min_temp[:14]:
+        mint.append(c.find(class_='unit unit_temperature_c').text)
 
-# Запись цсв файла, райтроу принимает только кортеж, получается записать только 1 столбец, нужно решить этот вопрос
 
-with open(f'{title}.csv','w',encoding='utf-8') as file:
-    writer = csv.writer(file)
-    writer.writerow(
-        (
-            ' День недели ',
-            " Дата ",
-            " Максимальная температура ",
-            " Минимальная температура ",
-            " Облачность" ,
-            " Ветер, м/c ",
-            ' Осадки,мм'
-        )
-    )
+
+
+    all_dates = all_dates[:14]
+    all_days = all_days[:14]
+    max_temp = max_temp[:14]
+
+    print ('\n\n\n',title)
     for i in range(14):
+        print (f'{all_days[i].text} {all_dates[i].text} t max = {maxt[i]} t min = {mint[i]} {conditions[i]} , ветер {wind[i]} м/c , осадки {precips[i]} мм')
+
+    # Запись цсв файла, райтроу принимает только кортеж, получается записать только 1 столбец, нужно решить этот вопрос
+
+    with open(f'{title}.csv','w',encoding='utf-8') as file:
+        writer = csv.writer(file)
         writer.writerow(
             (
-            all_days[i].text,
-            all_dates[i].text,
-            maxt[i],
-            mint[i],
-            conditions[i],
-            wind[i],
-            precips[i]
+                ' День недели ',
+                " Дата ",
+                " Максимальная температура ",
+                " Минимальная температура ",
+                " Облачность" ,
+                " Ветер, м/c ",
+                ' Осадки,мм'
             )
         )
+        for i in range(14):
+            writer.writerow(
+                (
+                all_days[i].text,
+                all_dates[i].text,
+                maxt[i],
+                mint[i],
+                conditions[i],
+                wind[i],
+                precips[i]
+                )
+            )
 
